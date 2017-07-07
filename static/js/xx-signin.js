2ee0b67ce324833684ee9973c55aab6c49d90abf
wagagaa <wwww.lklklk@qq.com>
2017-07-07 09:28:21 +0800

    0

--- static/js/xx-signin.js
+++ static/js/xx-signin.js
@@ -28,4 +28,52 @@ $(function(){
 		}
 	})
 //	$("form div").html("")
+
+
+	$('#infos').validate({
+		 debug:false, //false表示验证通过后不要自动提交表单
+		 onkeyup:false, //表示关闭按键松开时候监听验证
+		 rules:{ //验证规则
+			 username:{
+				 required:true, //必填字段
+				 rangelength:[6,100], //长度在6-10之间
+				 digits:true
+			 },
+			 nums:{
+				 required:true,
+				 digits:true,
+				 rangelength:[11,11],
+			 },
+			 ver:{
+			 	required:true,
+			 	rangelength:[4,4],
+			 },
+			 pass:{
+			 	rangelength:[8,16],
+			 }
+		 },
+		 messages:{ //自定义错误信息，默认为英文，除了在这里配置以为，还可以通过配置文件进行配置
+			 username:{
+				 required:"用户名必填",
+				 rangelength:"用户名长度必须为6-10位",
+				 email:"输入正确的邮箱账号"
+				 
+			 },
+			 nums:{
+				 required:"工作号必填",
+				 digits:"工作号必须为数字",
+				 rangelength:"输入正确手机号"
+			 },
+			 ver:{
+			 	rangelength:"输入4位验证码",
+			 },
+			 pass:{
+			 	 rangelength:"用户名长度必须为8-18位",
+			 }
+		},
+			 //验证通过可以在这里做你想做的事情
+		submitHandler:function(form){
+			 	alert("验证通过");
+			 }
+	})
 })
